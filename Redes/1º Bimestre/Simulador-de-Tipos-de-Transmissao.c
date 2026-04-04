d01bac097248bad8ca5672adbab565758fafb1d3
Luiz Dousseau <Luiznewbie@gmail.com>
2026-04-04 18:33:09 +0000

    Add files via upload

--- Redes/1º Bimestre/Simulador-de-Tipos-de-Transmissao.c
+++ Redes/1º Bimestre/Simulador-de-Tipos-de-Transmissao.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	int quantidade, escolha, multi, resul;
-printf("Digite a quantidade de pacotes: \n");
-scanf("%i", &quantidade);
-
-printf("Escolha o tipo de transmissao: \n");
+    int escolha;
+    float porcento, quantidade, resul, multi;
+ printf("Digite a quantidade de pacotes: \n");
+scanf("%f", &quantidade);
+while (1) {
+    printf("\nEscolha o tipo de transmissao: \n");
 printf("Digite 1 para Unicast \n");
-printf("Digite 2 para Multicast \n");
-printf("Digite 3 para Broadcast \n");
-scanf("%i", &escolha);
-
-if(escolha == 1) {
-printf("O numero de pacotes enviados e: 1 \n");
+    printf("Digite 2 para Multicast \n");
+     printf("Digite 3 para Broadcast \n");
+    printf("Digite 4 para fechar \n");
+    scanf("%i", &escolha);
+if (escolha == 1) {
+printf("\nO numero de pacotes enviados e: 1 \n");
+} else if (escolha == 2) {
+            printf("Digite a quantidade de dispositivos: \n");
+            scanf("%f", &multi);
+resul = quantidade - multi;
+porcento = (multi / quantidade) * 100;
+printf("\nO numero de pacotes perdidos e: %.2f \n", resul);
+            printf("O numero de pacotes recebidos e: %.2f \n", multi);
+            printf("O nivel de eficiencia da transmissao e: %.2f%% \n", porcento);
+} else if (escolha == 3) {
+            printf("\nO numero de pacotes enviados e: %f \n", quantidade);
+} else if (escolha == 4) {
+            break;
+        } else {
+            printf("Digite um numero valido!!!\n");
+        }
+    }
+return 0;
 }
-
-else if(escolha == 2) {
-	printf("Digite a quantidade de dispositivos: \n");
-	scanf("%i", &multi);
-	resul = quantidade - multi;
-printf("O numero de pacotes perdidos e: %i \n", resul);
-printf("O numero de pacotes recebidos e: %i \n", multi);
-}
-
-else {
-	printf("O numero de pacotes enviados e: %i \n", quantidade);
-}
-	return 0;
-}
-
